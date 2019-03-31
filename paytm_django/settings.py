3a0c148623a354370089afca63e2ff1556631f7a
Shubham Jaswal <shubhamjaswal772@gmail.com>
2019-03-31 17:27:06 +0530

    secure ssl

--- paytm_django/settings.py
+++ paytm_django/settings.py
@@ -148,3 +148,14 @@ PAYTM_CALLBACK_URL = "response/"
 #     Bank : Andhra Bank
 #     User : test
 #     Password : test
+
+
+# CORS_REPLACE_HTTPS_REFERER      = False
+# HOST_SCHEME                     = "http://"
+# SECURE_PROXY_SSL_HEADER         = None
+# SECURE_SSL_REDIRECT             = False
+# SESSION_COOKIE_SECURE           = False
+# CSRF_COOKIE_SECURE              = False
+# SECURE_HSTS_SECONDS             = None
+# SECURE_HSTS_INCLUDE_SUBDOMAINS  = False
+# SECURE_FRAME_DENY               = False
